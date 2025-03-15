2d5ca75e7598e191f0962f3fc45c963e2f6ea9d5
Ewerton Jose <ewerton.j.c.m@gmail.com>
2025-03-15 15:12:02 -0300

    Last Commit

--- Blue Gravity Project/Assets/Game/Scripts/Player/Scr_UI_PlayerInformations.cs
+++ Blue Gravity Project/Assets/Game/Scripts/Player/Scr_UI_PlayerInformations.cs
@@ -6,13 +6,11 @@ public class Scr_UI_PlayerInformations : MonoBehaviour
 {
     [SerializeField] private Scr_SO_PlayerInformations _playerInformations;
 
-    [SerializeField] private TextMeshProUGUI _maxHealth;
     [SerializeField] private TextMeshProUGUI _currentHealth;
     [SerializeField] private TextMeshProUGUI _playerCurrentMoney;
 
     private void Start()
     {
-        _maxHealth.text = "Max Health: " + _playerInformations.MaxHealth;
         _currentHealth.text = "Current Health: " + Scr_Manager_GameManager.Instance.PlayerHealth;
 
         _playerCurrentMoney.text = "Current Money: " + Scr_Manager_GameManager.Instance.PlayerMoney;
